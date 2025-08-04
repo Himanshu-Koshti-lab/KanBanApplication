c2814b02575967a412c3dc456773aed8fb2cb129
Himanshu Koshti <Himfeedback74@gmail.com>
2025-08-04 20:59:55 +0530

    US-6 Added Logger in application.

--- src/main/java/com/kanban/pro/kanban_tool/controller/Dashboard.java
+++ src/main/java/com/kanban/pro/kanban_tool/controller/Dashboard.java
@@ -1,12 +1,14 @@
 package com.kanban.pro.kanban_tool.controller;
 
 import com.kanban.pro.kanban_tool.service.UserStoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class Dashboard {
 
     @Autowired
@@ -14,6 +16,7 @@ public class Dashboard {
 
     @GetMapping("/dashboard")
     public String dashboard(Model model) {
+        log.info("Loading Dashboard...");
         model.addAttribute("stories", userStoryService.getUserStories());
         return "userstory";
     }
--- src/main/java/com/kanban/pro/kanban_tool/controller/UserStoryController.java
+++ src/main/java/com/kanban/pro/kanban_tool/controller/UserStoryController.java
@@ -2,12 +2,14 @@ package com.kanban.pro.kanban_tool.controller;
 
 import com.kanban.pro.kanban_tool.entity.UserStory;
 import com.kanban.pro.kanban_tool.service.UserStoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1/api/")
 public class UserStoryController {
@@ -22,6 +24,7 @@ public class UserStoryController {
     @PostMapping("create")
     public ResponseEntity<String> createStory(@RequestParam("title") String title, @RequestParam("desc") String desc){
         try {
+            log.info("Creating story with title: {} and desc: {}", title, desc);
             return userStoryService.create(title, desc);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage() , HttpStatus.BAD_REQUEST);
@@ -30,22 +33,26 @@ public class UserStoryController {
 
     @GetMapping("getUserStories")
     public List<UserStory> getAllStories() {
+        log.info("Getting all user stories");
         return userStoryService.getUserStories();
     }
 
     @GetMapping("getUserStory/{id}")
     public UserStory getStory(@PathVariable("id") Long id) {
+        log.info("Getting user story with id: {}", id);
         return userStoryService.getUserStoryById(id);
     }
 
     @PostMapping("update")
     public ResponseEntity<UserStory> updateStory(@RequestBody UserStory userStory){
+        log.info("Updating user story with id: {}", userStory.getId());
         return userStoryService.updateStory(userStory);
     }
 
 
     @PostMapping("updateStatus")
     public ResponseEntity<String> updateStoryStatus( @RequestParam Long id, @RequestParam String status){
+        log.info("Updating user story Status {} with id: {}", status, id);
         return userStoryService.updateStoryStatus(id, status);
     }
 
