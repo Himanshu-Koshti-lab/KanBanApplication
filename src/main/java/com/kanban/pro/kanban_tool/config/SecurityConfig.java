36d8876b74c06336aa4b7d7a04ef6af31d18bb84
Himanshu Koshti <Himfeedback74@gmail.com>
2025-08-06 22:49:34 +0530

    US-12 Added Spring Security with Basic Auth.

---
+++ src/main/java/com/kanban/pro/kanban_tool/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.kanban.pro.kanban_tool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .logout(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/dashboard","/v1/api/getUserStories").permitAll()
+                        .anyRequest().authenticated());
+
+        return httpSecurity.build();
+    }
+}
